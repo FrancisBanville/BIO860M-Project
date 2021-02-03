d2f46f32ed58db53fb6237f3e925ff8048091025
FrancisBanville <francis.banville@umontreal.ca>
2021-02-03 11:49:52 -0500

    instantiate project

---
+++ main.jl
@@ -0,0 +1,11 @@
+# Activate project environment
+import Pkg; Pkg.activate(".")
+
+Pkg.instantiate()
+
+# Load required packages
+using BioEnergeticFoodWebs
+using EcologicalNetworks
+using Mangal
+
+# Load scripts
